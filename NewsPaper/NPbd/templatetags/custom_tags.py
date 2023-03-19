6c8b1d3169f330f86bfbff0c7763d549d38d6d27
GennadyIG <gavrilenkogi@mail.ru>
2023-03-19 21:44:19 +0300

    Fixed access to the buttons for editing and deleting posts

--- NewsPaper/NPbd/templatetags/custom_tags.py
+++ NewsPaper/NPbd/templatetags/custom_tags.py
@@ -1,6 +1,8 @@
 from django import template
+from django.contrib.auth.models import Group
 
 register = template.Library()
+
 
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
@@ -8,3 +10,13 @@ def url_replace(context, **kwargs):
     for k, v in kwargs.items():
         d[k] = v
     return d.urlencode()
+
+
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    try:
+        group =  Group.objects.get(name=group_name)
+    except Group.DoesNotExist:
+        return False
+
+    return group in user.groups.all()
