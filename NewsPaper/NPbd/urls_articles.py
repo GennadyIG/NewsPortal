823f254721aa1a83452eb5eae912ce0353ee5ae7
GennadyIG <gavrilenkogi@mail.ru>
2023-10-28 15:47:57 +0300

    Fix translate

--- NewsPaper/NPbd/urls_articles.py
+++ NewsPaper/NPbd/urls_articles.py
@@ -3,7 +3,7 @@ from .views import (PostList, PostDetail, ArticleCreate, ArticleUpdate, ArticleD
 from django.views.decorators.cache import cache_page
 
 urlpatterns = [
-    path('', cache_page(60)(PostList.as_view()), name='post_list'),
+    path('', PostList.as_view(), name='post_list'),
     path('<int:pk>', PostDetail.as_view(), name='article_detail'),
     path('create/', ArticleCreate.as_view(), name='article_create'),
     path('<int:pk>/edit/', ArticleUpdate.as_view(), name='article_edit'),
--- NewsPaper/NPbd/urls_news.py
+++ NewsPaper/NPbd/urls_news.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 from .views import (PostList, PostDetail, NewsCreate, NewsUpdate, NewsDelete, like)
 
 urlpatterns = [
-    path('', cache_page(60)(PostList.as_view()), name='post_list'),
+    path('', PostList.as_view(), name='post_list'),
     path('<int:pk>', PostDetail.as_view(), name='news_detail'),
     path('create/', NewsCreate.as_view(), name='news_create'),
     path('<int:pk>/edit/', NewsUpdate.as_view(), name='news_edit'),
